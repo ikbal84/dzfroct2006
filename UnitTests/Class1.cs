424ac3b0f8e41e647c48843310ee7b75884c20e1
ikbal84 <ikbal84@gmail.com>
2015-01-31 14:54:49 +0100

    Project Init

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        
+        [Test]
+        public void test1()
+        {
+            Assert.IsTrue(false);
+        }
+
+    }
+}
---
+++ dzfroct2006/UnitTests/NUnitTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class NUnitTestFixture
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
+
