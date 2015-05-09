1205aa5dc944fd5011c58f8988a12cb6ced39df4
unknown <senhiam16@gmail.com>
2015-05-09 21:10:47 +0200

    Resolution conflis version MVS

--- dzfroct2006/DAL/CitiesDAO.cs
+++ dzfroct2006/DAL/CitiesDAO.cs
@@ -19,7 +19,7 @@ namespace dzfroct2006.DAL
 
             // Read the file and display it line by line.
             System.IO.StreamReader file =
-                new System.IO.StreamReader(@"C:\Users\Moh\Documents\Visual Studio 2012\Projects\dzfroct2006\dzfroct2006\App_GlobalResources\codes_communes.csv");
+                new System.IO.StreamReader(@"D:\projets\2015\HotelOnLineAlgeria\tmp\dzfroct2006\dzfroct2006\App_GlobalResources\codes_communes.csv");
             while ((line = file.ReadLine()) != null)
             {
                 string[] words = line.Split(';');
